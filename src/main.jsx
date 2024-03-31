e06f0793d67f5040c04e128e5bb98c0f16165c40
Oksana Marusich <oksana.m229@gmail.com>
2024-03-31 20:27:51 +0300

    add folders components, pages, redux, utils,hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+// import { PersistGate } from 'redux-persist/integration/react'
+import { ThemeProvider } from "styled-components";
+import { App } from "./App.jsx";
+import { GlobalStyle } from "./components/GlobalStyle.js";
+// import { persistor, store } from './redux/store.js'
+import { theme } from "./styles/theme.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <Provider store={store}>
+     <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter basename="/">
+      <ThemeProvider theme={theme}>
+        <App />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
+    {/* </PersistGate>
+      </Provider> */}
+  </React.StrictMode>
+);
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+    return (<>
+        <h1 style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}>Hello World</h1>
+      </>
+    )
+};
