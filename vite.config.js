db0a7892e46863321e1e28023c452ee3344b7fdc
Nikita_Beliy <beliynikita1995@gmail.com>
2024-03-31 13:37:37 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import viteSvgrPlugin from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [react(), viteSvgrPlugin({ include: "**/*.svg?react" })],
+  server: {
+    open: true,
+    port: 3000,
+  },
+});
