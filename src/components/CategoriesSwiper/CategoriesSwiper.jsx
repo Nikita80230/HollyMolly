0f482a4b17c5f2ae9961f3bcaad91a38fee36332
Oksana Marusich <oksana.m229@gmail.com>
2024-04-17 13:19:44 +0300

    add swiper-mobile

--- src/components/CategoriesSwiper/CategoriesSwiper.jsx
+++ src/components/CategoriesSwiper/CategoriesSwiper.jsx
@@ -1,9 +1,10 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, A11y } from "swiper/modules";
+import { Grid, Navigation } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/grid";
 import "swiper/css/navigation";
+
 import { StyledCategoriesSection } from "./Styled";
-// import { CardCategories } from "../CardCategories/CardCategories";
 import ArrowLeft from "src/assets/images/arrowLeft.svg?react";
 import ArrowRight from "src/assets/images/arrowRight.svg?react";
 import CardCategories from "../CardCategories/CardCategories";
@@ -12,54 +13,59 @@ const CategoriesSwiper = () => {
   return (
     <StyledCategoriesSection>
       <h2 className="titleCategories">Категорії</h2>
-      <Swiper
-        modules={[Navigation, A11y]}
-        spaceBetween={24}
-        slidesPerView={4}
-        navigation={{
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        }}
-        breakpoints={{
-          320: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-          1024: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-          },
-        }}
-      >
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCategories />
-        </SwiperSlide>
-      </Swiper>
+      <div className="swiper-wrapper">
+        <Swiper
+          modules={[Grid, Navigation]}
+          grid={{
+            rows: 2,
+            fill: "column",
+          }}
+          breakpoints={{
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            // 678: {
+            //    slidesPerView: 2,
+            //   spaceBetween: 20,
+            // },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 24,
+              grid: 1,
+            },
+          }}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+        >
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCategories />
+          </SwiperSlide>
+        </Swiper>
+      </div>
       <div className="button-swiper">
         <button className="swiper-button-next" type="button">
           <ArrowRight />
--- src/components/CategoriesSwiper/Styled.js
+++ src/components/CategoriesSwiper/Styled.js
@@ -2,41 +2,71 @@ import { styled } from "styled-components";
 
 export const StyledCategoriesSection = styled.section`
   position: relative;
-  margin-bottom: 80px;
+  margin-bottom: 36px;
+
+  @media only screen and (min-width: 768px) {
+    margin-bottom: 80px;
+  }
 
   .titleCategories {
     margin-bottom: 40px;
     font-family: ${({ theme }) => theme.fonts.fontNunito};
-    font-weight: 600;
-    font-size: 32px;
+    font-weight: 500;
+    font-size: 20px;
     color: ${({ theme }) => theme.colors.primaryTxtColor};
+
+    @media only screen and (min-width: 768px) {
+      font-weight: 600;
+      font-size: 32px;
+    }
   }
-  
+
+  .swiper-wrapper {
+    width: 280px;
+    height: 356px;
+    @media only screen and (min-width: 768px) {
+      width: 1312px;
+      height: 294px;
+    }
+  }
+
   .button-swiper {
     position: absolute;
     top: 0;
     right: 0;
-    width: 132px;
-    height: 44px;
+    width: 90px;
+    height: 31px;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    gap:24px;
+    gap: 15px;
+
+    @media only screen and (min-width: 768px) {
+      width: 132px;
+      height: 44px;
+      gap: 24px;
+    }
   }
   .swiper-button-next,
   .swiper-button-prev {
-    padding: 10px;
-    width: 44px;
-    height: 44px;
+    padding: 7px;
+    width: 31px;
+    height: 31px;
     display: flex;
     justify-content: center;
     align-items: center;
     background-color: transparent;
-    border: 1px solid ${({theme})=>theme.colors.borderButton};
+    border: 1px solid ${({ theme }) => theme.colors.borderButton};
     border-radius: 8px;
+
+    @media only screen and (min-width: 768px) {
+      padding: 10px;
+      width: 44px;
+      height: 44px;
+    }
   }
   .swiper-button-next:after,
   .swiper-button-prev:after {
-    display:none;
+    display: none;
   }
 `;
