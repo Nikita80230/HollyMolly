fabaacf1ab0168428ba2794e5d77ee272996841c
Oksana Marusich <oksana.m229@gmail.com>
2024-05-13 22:15:38 +0300

    fix position Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,17 @@
 import { Hearts } from "react-loader-spinner";
+import { WrapperLoader } from "./Styled";
 
 const Loader = () => {
   return (
-    <Hearts
-      height="100"
-      width="100"
-      color="#98714A"
-      ariaLabel="hearts-loading"
-      wrapperStyle={{
-        position: "absolute",
-        top: "40%",
-        left: "40%",
-        zIndex: "103",
-      }}
-      // wrapperClass=""
-      visible={true}
-    />
+    <WrapperLoader>
+      <Hearts
+        height="100"
+        width="100"
+        color="#98714A"
+        ariaLabel="hearts-loading"
+        visible={true}
+      />
+    </WrapperLoader>
   );
 };
 
