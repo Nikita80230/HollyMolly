ebbbd00d042c467bcdc653084e45d414a59b1464
Oksana Marusich <oksana.m229@gmail.com>
2024-09-25 22:59:54 +0300

    add

--- src/components/TitlePage/TitlePage.jsx
+++ src/components/TitlePage/TitlePage.jsx
@@ -1,8 +1,11 @@
 import { StyledTitle } from "./Styled";
 
-const TitlePage = ({title}) => {
-   
-    return (<StyledTitle>{title}</StyledTitle>)
-}
+const TitlePage = ({ title, width, height }) => {
+  return (
+    <StyledTitle width={width} height={height}>
+      {title}
+    </StyledTitle>
+  );
+};
 
 export default TitlePage;
