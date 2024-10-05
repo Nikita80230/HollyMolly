3744c0133354f7759041dba6824b9c3508159188
Oksana Marusich <oksana.m229@gmail.com>
2024-10-05 23:37:23 +0300

    fix get orderId

--- src/redux/orders/selectors.js
+++ src/redux/orders/selectors.js
@@ -3,3 +3,5 @@ export const selectCustomer = (state) => state.orders.customer;
 export const selectOrderId = (state) => state.orders.orderId;
 export const selectIsLoading = (state) => state.orders.isLoading;
 export const selectMyOrders = (state) => state.orders.myOrders;
+
+console.log("selectors",selectOrderId);
